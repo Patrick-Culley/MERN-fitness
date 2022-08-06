4ebe9e581b39f04b0abff9785451befc055ac0ad
patbc <culleyp@oregonstate.edu>
2022-08-06 01:09:38 -0500

    update controller.mjs and add .env file

---
+++ back-rest/controller.mjs
@@ -0,0 +1,15 @@
+import 'dotenv/config'; 
+import express from 'express';
+
+const PORT = process.env.PORT; 
+const app = express(); 
+app.use(express.json()); 
+
+// CREATE/POST controller *****************************************
+
+// GET controller *************************************************
+
+// UPDATE/PUT controller ******************************************
+
+// DELETE controller **********************************************
+
